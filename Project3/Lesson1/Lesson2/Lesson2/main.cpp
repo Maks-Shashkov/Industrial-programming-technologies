3c09050b2c33efec5de3da5913180d93439ab61e
Максим Шашков <88987688+Maks-Shashkov@users.noreply.github.com>
2023-03-05 21:08:09 +0300

    Add files via upload

---
+++ Project3/Lesson1/Lesson2/Lesson2/main.cpp
@@ -0,0 +1,35 @@
+#include "Header.h"
+int main()
+{
+    setlocale(0, "");
+    int n, num, dem;
+    while (true)
+    {
+        system("cls");
+        cout << "Введите количество дробей: ";
+        cin >> n;
+        if (n == 0)
+        {
+            return false;
+        }
+        rational* mas = new rational[n];
+        system("cls");
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Числитель: ";
+            cin >> num;
+            cout << "Знаменатель: ";
+            cin >> dem;
+            mas[i].set(num, dem);
+            mas[i] = rational(num, dem);
+            system("cls");
+        }
+        for (int i = 0; i < n; i++)
+        {
+            mas[i].show();
+        }
+        delete[]mas;
+        system("pause");
+    }
+    return 0;
+}
